12ef0b77d7518887c380e8abbce6cd66c4276a55
Kousthubh Yadavalli <kyadavalli04@gmail.com>
2023-03-13 14:04:10 +0530

    loaded images

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,27 +5,20 @@ import './styles/Categories.css';
 function Categories() {
   const [categories, setCategories] = useState([]);
   const [imageURL, setImageURL] = useState('');
+ 
   useEffect(() => {
     fetch('http://localhost:8000/Category/')
-    .then((response) => response.json())
-    .then((data) => {
-      setCategories(data);
-      return data;
-    })
-    .then((data) => fetch(data[0].image))
-    .then((response) => response.blob())
-    .then((blob) => setImageURL(URL.createObjectURL(blob)))
-    .catch((error) => console.log(error));
-  
+      .then((response) => response.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.log(error));
   }, []);
-
   return (
     <div className="container">
       <div className="row justify-content-center">
         {categories.map((category) => (
-          <div className="containercategory">
-            <div className="col-md-6 col-lg-4 mb-4" key={category.id}>
-              <Category_card key={category.id} name={category.name} img={category.image} />
+          <div className="containercategory" key={category.id}>
+            <div className="col-md-6 col-lg-4 mb-4">
+              <Category_card name={category.name} imageURL={category.image} />
             </div>
           </div>
         ))}
@@ -35,3 +28,4 @@ function Categories() {
 }
 
 export default Categories;
+
--- src/components/sub-components/Category_card.js
+++ src/components/sub-components/Category_card.js
@@ -2,22 +2,21 @@ import React from 'react'
 import {Link} from "react-router-dom"
 
 // this is the card that displays categories
-function Category_card({name,img}) {
+function Category_card({ name, imageURL }) {
+  const urlWithBase=`http://localhost:8000${imageURL}`;
   return (
     // using card from bootstrap
     <div className='my-4'>
       <div className="card" style={{width: '18rem',borderRadius:"15px"}}>
-  <img  className="card-img-top" src={img} alt="this is an image"/>
-  <div className="card-body">
-    <h5 className="card-title">{name}</h5>
-    <Link className="btn btn-primary my-2 mx-2" to={`/Items/${name}`}>See items</Link>
-  </div>
-</div>
+        <img className="card-img-top" src={urlWithBase} alt="this is an image"/>
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <Link className="btn btn-primary my-2 mx-2" to={`/Items/${name}`}>See items</Link>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Category_card
-// add the proptypes here
-
 
